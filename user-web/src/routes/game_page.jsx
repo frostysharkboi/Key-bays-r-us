1b622f3e896533a931e88dd14b996919d7e37106
P0STAL-DUDE <ignacykaminski07@gmail.com>
2026-04-20 19:35:33 +0200

    game_page po części działa. Muszę jeszcze dodać tagi oraz recenzje.

--- user-web/src/routes/game_page.jsx
+++ user-web/src/routes/game_page.jsx
@@ -9,6 +9,7 @@ import './root.css';
 export default function Root(){
 
   const [game, setGame] = useState([]);               // Dane gier z bazy danych
+  
 
   const navigate = useNavigate();
   const location = useLocation();
@@ -23,6 +24,8 @@ export default function Root(){
       setGame(res.data);
     });
   };
+
+  
   React.useEffect(() => {
     getGame();
   }, []);
@@ -62,7 +65,7 @@ export default function Root(){
       {/* Tytuł Gry */}
       <div className='row m-3 p-3 text-center'>
         <div className='col'>
-          <h2>{(game[0])?  game[0].title : "Title"}</h2>
+          <h2>{(game[0])?  game[0].title : "Nie znaleziono"}</h2>
         </div>
       </div>
 
@@ -70,7 +73,7 @@ export default function Root(){
       <div className='row m-3 p-3 text-center'>
         {/* Media */}
         <div className='col-7'>
-          <iframe src="https://www.youtube.com/watch?v=Yr4Skj4Gc4E" width="800px" heigth="600px"/>
+          <img src={(game[0])?  game[0].cover_img : "Nie znaleziono"}/>
         </div>
         <div className='col-5'>
             {/* Recenzje - średnia w gwiazdkach */}
@@ -86,7 +89,8 @@ export default function Root(){
             <br/>
             {/* Producent */}
             <div>
-              <p>Producent</p>
+              <p>Data Wydania: {(game[0])?  game[0].release_date : "Nie znaleziono"}</p>
+              <p>Developer: {(game[0])?  game[0].publisher : "Nie znaleziono"}</p>
             </div>
         </div>
       </div>
@@ -95,20 +99,34 @@ export default function Root(){
       <div className='row m-3 p-3 text-center'>
         {/* Opis Gry */}
         <div className='col-7'>
-          <p>Opis gry</p>
+          <p>{(game[0])?  game[0].about : "Nie znaleziono"}</p>
         </div>
         {/* Div z obydwoma wymaganiami */}
-        <div className='col-5'>
+        <div className='col-5 d-flex'>
           {/* Zalecane */}
-          <div>
-            <p>Zalecane wymagania</p>
+          <div className='m-4'>
+            <h3>Zalecane Wymagania</h3>
+            <p>
+              System Operacyjny: {(game[0])?  game[0].opt_os : "Nie znaleziono"}<br/>
+              Karta Graficzna: {(game[0])?  game[0].opt_gpu : "Nie znaleziono"}<br/>
+              Procesor: {(game[0])?  game[0].opt_cpu : "Nie znaleziono"}<br/>
+              Pamięć ram: {(game[0])?  game[0].opt_ram : "Nie znaleziono"} GB<br/>
+              Potrzebne miejsce: {(game[0])?  game[0].opt_size : "Nie znaleziono"} GB
+            </p>
           </div>
 
           <br/>
 
           {/* Minimalne */}
-          <div>
-            <p>Minimalne wymagania</p>
+          <div className='m-4'>
+            <h3>Minimalne Wymagania</h3>
+            <p>
+              System Operacyjny: {(game[0])?  game[0].min_os : "Nie znaleziono"}<br/>
+              Karta Graficzna: {(game[0])?  game[0].min_gpu : "Nie znaleziono"}<br/>
+              Procesor: {(game[0])?  game[0].min_cpu : "Nie znaleziono"}<br/>
+              Pamięć ram: {(game[0])?  game[0].min_ram : "Nie znaleziono"} GB<br/>
+              Potrzebne miejsce: {(game[0])?  game[0].min_size : "Nie znaleziono"} GB
+            </p>
           </div>
         </div>
       </div>
