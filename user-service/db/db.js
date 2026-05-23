f2f347c1947b5fd64c5e77966e59afadc323ea4e
Sekretarz <jakubchmiel2007@gmail.com>
2026-05-23 20:20:30 +0200

    Added external host as database source

--- user-service/db/db.js
+++ user-service/db/db.js
@@ -1,15 +1,36 @@
 const mariadb = require("mariadb");
 
-//connect to mariadb
+/*Wersja Lokalnie* /
 
 const pool = mariadb.createPool({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"keybay", //BAZA DANYCH
+    host:"serwer2670396.hosting-home.pl",
+    user:"41188776_keysrus",
+    password:"keysrus1234",
+    database:"41188776_keysrus", //BAZA DANYCH
     port:3306
 });
 
 module.exports=Object.freeze(
     {pool:pool}
 );
+
+const mariadb = require("mariadb");
+/* Wersja Na Hoscie */
+const pool = mariadb.createPool({
+    host: "serwer2670396.hosting-home.pl",
+    user: "41188776_keysrus",
+    password: "keysrus1234", //na hostingu: o42mkdEmk //użytkownika: keysrus1234
+    database: "41188776_keysrus",
+
+    // home.pl MySQL 8
+    port: 3380,
+
+    connectionLimit: 5,
+
+    ssl: false
+});
+
+module.exports = Object.freeze({
+    pool
+});
+/**/
